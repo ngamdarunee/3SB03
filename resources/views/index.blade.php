87373476b348436ea222541988f3899f156f36ad
ngamdarunee <s6135512038@phuket.psu.ac.th>
2021-02-12 00:40:05 +0700

    แลป3

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+        <title>6135512038</title>
+</head>
+
+<body style="background-color:pink">  
+
+        <h2 style="text-align:center ; color:white">Send E-mail</h2>
+
+        <form style="text-align:center ; color:white" name="form1" method="get" action="/sendbasicemail">  
+                Name: <input type="text" name="name"  placeholder="name"><br><br>
+                E-mail: <input type="email" name="email"  placeholder="email"><br><br>
+                Message : <input type="text" name="message"  placeholder="message"  ><br><br>
+                <input type="submit" name="submit" value="Submit">  
+        </form>
+       
+</body>
+</html>
